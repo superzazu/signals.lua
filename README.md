890248840343685953f6f6f6ee87027f9a661f02
SuperZazu <nico.g.allemand@gmail.com>
2015-05-26 22:41:00 +0200

    added tests

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # signals
-
 signals is a simple signals module for Lua.
 
-## lel
+## Simple use
+```
+local signals = require 'signals'
+
+local monster = {}
+monster.is_alive = true
+
+local function my_callback(sender)
+    sender.is_alive = false
+end
+
+signals.connect('hero_hit_monster', my_callback)
+signals.send('hero_hit_monster', monster)
+```
--- signals.lua
+++ signals.lua
@@ -1,29 +1,45 @@
-local signals = {}
+local signals = {
+    _VERSION         = 'signals v1.0.0',
+    _DESCRIPTION    = 'An simple signals module in Lua',
+    _URL            = 'http://github.com/superzazu/signals.lua',
+    _LICENSE        = [[
+    Copyright (c) 2014 Nicolas Allemand
 
-local s = {}
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+    ]]
+}
+
+local _s = {}
 
 signals.connect = function (signal_name, callback)
-    if not s[signal_name] then
-        s[signal_name] = {}
-    end
-    s[signal_name][#s[signal_name]+1] = callback
+    _s[signal_name] = callback
 end
 
 signals.disconnect = function (signal_name, callback)
-    if s[signal_name] then
-        for i=1,#s[signal_name] do
-            if s[signal_name][i] == callback then
-                s[signal_name][i] = function () end
-            end
-        end
+    if _s[signal_name] then
+        _s[signal_name] = nil
     end
 end
 
 signals.send = function (signal_name, sender, ...)
-    if s[signal_name] then
-        for i=1,#s[signal_name] do
-            s[signal_name][i](sender, ...)
-        end
+    if _s[signal_name] then
+        _s[signal_name](sender, ...)
     end
 end
 
